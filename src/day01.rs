8f241819282f6d6dddabaad37b588300f95b8692
Stephen Bennett <bennett18@gmail.com>
2019-12-07 19:22:27 -0500

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -9,6 +9,24 @@ pub fn original_1a(input: &str) -> i32 {
         .sum()
 }
 
+#[aoc(day01, part2, original)]
+pub fn original_1b(input: &str) -> i32 {
+    let modules = input.lines().map(|l| l.trim().parse::<i32>().unwrap());
+    let mut total_fuel: i32 = 0;
+    for mass in modules {
+        let mut fuel = compute_fuel_required(mass);
+        while fuel != 0 {
+            total_fuel += fuel;
+            fuel = compute_fuel_required(fuel);
+        }
+    }
+    total_fuel
+}
+
+fn compute_fuel_required(mass: i32) -> i32 {
+    cmp::max((mass / 3) - 2, 0)
+}
+
 #[cfg(test)]
 mod tests {
     use day01::original_1a;
